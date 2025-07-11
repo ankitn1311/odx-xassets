d0d0196fd9b2811af4d3521089de41896cb46930
ankitn1311 <ankitn1311@gmail.com>
2025-07-11 16:32:59 +0530

    fix: width of the xAsset screen

--- app/(main)/x-assets/page.tsx
+++ app/(main)/x-assets/page.tsx
@@ -4,7 +4,7 @@ import { TokenSwapCard } from '@/components/x-assets/TokenSwapCard';
 
 export default function XAssets() {
   return (
-    <div className="mx-auto grid w-full max-w-6xl gap-4 p-4 md:grid-cols-[1fr_400px]">
+    <div className="mx-auto grid w-full max-w-7xl gap-4 p-4 md:grid-cols-[1fr_400px]">
       <TokenChart />
       <TokenSwapCard />
     </div>
