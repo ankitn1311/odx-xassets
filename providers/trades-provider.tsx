6561a96f453e8f8759b83c7d406932c792da9c10
ankitn1311 <ankitn1311@gmail.com>
2025-08-11 01:16:06 +0530

    feat: order id added

--- providers/trades-provider.tsx
+++ providers/trades-provider.tsx
@@ -15,6 +15,7 @@ export interface TradeData {
   usdAmount: number;
   side: string;
   tradeId: string;
+  orderId?: string;
 }
 
 export interface WebSocketMessage {
@@ -28,6 +29,7 @@ export interface WebSocketMessage {
   time: string;
   side: string;
   tradeId: string;
+  orderId?: string;
 }
 
 type TradesContextType = {
@@ -43,7 +45,7 @@ interface TradesProviderProps {
 
 const TradesContext = createContext<TradesContextType | null>(null);
 
-const WEB_SOCKET_URL = 'wss://ashwin-ap-northeast-1.broadcastservice.api.devo.backend.odx.so/devo';
+const WEB_SOCKET_URL = process.env.NEXT_PUBLIC_WSS_BASE_URL as string;
 export const TradesProvider: React.FC<TradesProviderProps> = ({ children }) => {
   const { address } = useAccount();
   const queryClient = useQueryClient();
